ff02fc59981398fd75ccd49abaaf207d05b4e80c
“Zayd <zsaid735@gmail.com>
2023-03-22 12:32:37 +0000

    debugging and npm i mongoose ran

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const routes = require("./routes")
-const db = require("./config/connection")
+const db = require("./config/connections")
 
 const app = express()
-const PORT = process.enc.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({extended: true}))
 
 app.use(routes)
 
